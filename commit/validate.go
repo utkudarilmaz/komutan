253d18be9a4df66e21ab0129d54f4f4125aa98d0
Utku Darilmaz <utkudarilmaz@gmail.com>
2019-11-18 00:36:37 +0300

    feat(validate): validation the commit msg from given file function added

--- commit/validate.go
+++ commit/validate.go
@@ -2,6 +2,8 @@ package commit
 
 import (
 	"errors"
+	"io/ioutil"
+	"os"
 	"regexp"
 
 	logging "github.com/op/go-logging"
@@ -9,14 +11,39 @@ import (
 
 var (
 	log             = logging.MustGetLogger("base")
-	defaultTemplate = `^(feat|fix|refactor|chore)(\([a-zA-Z0-9]*-?[a-zA-z0-9]+\))?:\s[a-z].([a-zA-Z0-9\.',_-]|\s)+[^\s\.\!\?=_-]$`
+	defaultTemplate = `^(feat|fix|refactor|chore)(\([a-zA-Z0-9]*-?[a-zA-z0-9]+\))?:\s[a-z].([a-zA-Z0-9\.',_-]|\s)+[^\.\!\?=_-]$`
 )
 
-// Validate the given commit message with following RegExp.
-// ^(?=.{1,72}$)(feat|fix|refactor|chore)(|\([a-zA-Z0-9]+-?[a-zA-z0-9]+)\):\s[a-z].([a-zA-Z0-9\.,_-]|\s)+[^\.\!\?=_-]$
+// ValidateCommitMsgFile is validate the commit message where is given file
+func ValidateCommitMsgFile(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	buffer, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	if matched, err := regexp.Match(defaultTemplate, buffer); !matched {
+		if err != nil {
+			return err
+		}
+		return errors.New("\"" + string(buffer[:len(buffer)-1]) + "\"" + " commit message is not valid")
+	}
+
+	log.Notice("\"" + string(buffer[:len(buffer)-1]) + "\"" + " commit message is valid")
+
+	return nil
+}
+
+// ValidateCommitMsgString is validating the given commit message with following RegExp.
+// ^(feat|fix|refactor|chore)(\([a-zA-Z0-9]*-?[a-zA-z0-9]+\))?:\s[a-z].([a-zA-Z0-9\.,_-]|\s)+[^\.\!\?=_-]$
 // RegExp rules produced based a couple policies where the policies defined
 // https://www.conventionalcommits.org
-func Validate(message string) error {
+func ValidateCommitMsgString(message string) error {
 	if len(message) > 72 || len(message) < 7 {
 		return errors.New("message length must between 7 and 72 character")
 	}
