dd0a52718ee03ef00386a086bf8cabf90ab203ab
Utku Darilmaz <utkudarilmaz@gmail.com>
2019-12-24 01:38:13 +0300

    feat: release command name change to release-as
    --patch, --minor, --major flags added to release-as command

--- cmd/release.go
+++ cmd/release.go
@@ -7,45 +7,52 @@ import (
 )
 
 var (
-	Pre   bool
 	Patch bool
 	Minor bool
 	Major bool
 )
 
 func init() {
-	releaseCmd.Flags().BoolVarP(
+	releaseAsCmd.Flags().BoolVarP(
 		&Patch,
 		"patch",
 		"",
 		false,
-		"patch release",
+		"release as patch",
 	)
-	releaseCmd.Flags().BoolVarP(
+	releaseAsCmd.Flags().BoolVarP(
 		&Minor,
 		"minor",
 		"",
 		false,
-		"minor release",
+		"release as minor",
 	)
-	releaseCmd.Flags().BoolVarP(
+	releaseAsCmd.Flags().BoolVarP(
 		&Major,
 		"major",
 		"",
 		false,
-		"major release",
+		"release as major",
 	)
-	rootCmd.AddCommand(releaseCmd)
+	rootCmd.AddCommand(releaseAsCmd)
 }
 
-var releaseCmd = &cobra.Command{
-	Use:   "release",
-	Short: "create new release at current commit",
-	Args:  cobra.NoArgs,
+var releaseAsCmd = &cobra.Command{
+	Use:     "release-as",
+	Short:   "create new tag at head of repo",
+	Example: "komutan release-as patch # tags: v1.0.1 -> v1.0.1, v1.0.2",
+	Args:    cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		_, err := release.Patch()
-		if err != nil {
-			log.Errorf(err.Error())
+		if Patch {
+			err := release.Patch()
+			if err != nil {
+				log.Errorf(err.Error())
+			}
+		} else if Minor {
+			return
+		} else if Major {
+			return
 		}
+
 	},
 }
